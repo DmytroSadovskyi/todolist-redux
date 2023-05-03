8c1745a3dbd8cf5795a26d84ba96c1fe2d74d2df
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-05-03 16:42:09 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,14 @@
+import { AppBar } from './AppBar/AppBar';
+import { Layout } from './Layout/Layout';
+import { TodoForm } from './TodoForm/TodoForm';
+import { TodoList } from './TodoList/TodoList';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Layout>
+      <AppBar />
+      <TodoForm />
+      <TodoList />
+    </Layout>
   );
 };
